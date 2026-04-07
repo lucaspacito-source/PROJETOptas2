0404a7d8017c39a0137531327a8bf518b54929b7
lucaspacito-source <lucas.pacito@estudante.ifms.edu.br>
2026-04-07 00:41:07 -0300

    Adicionando frontend como diretório normal

---
+++ frontend/src/app/page.js
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-gray-100">
+      <Button variant="default">Clique aqui</Button>
+    </main>
+  );
+}
